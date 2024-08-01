a52881fd545ec7aea275b9fd34afd0b98f4445fc
Dhinesh <dhinesh342@gmail.com>
2024-08-02 02:57:53 +0530

    new charts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 function App() {
+  const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 300, pv: 2400, amt: 2400},
+    {name: 'Page C', uv: 300, pv: 2400, amt: 2400},{name: 'Page D', uv: 200, pv: 2400, amt: 2400},
+    {name: 'Page E', uv: 100, pv: 2400, amt: 2400},{name: 'Page F', uv: 300, pv: 2400, amt: 2400}
+];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+            <LineChart width={600} height={300} data={data}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="name" />
+                <YAxis />
+            </LineChart>
+        </div>
   );
 }
 
